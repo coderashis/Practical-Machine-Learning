4417a3f3a4830c8021d31d5fea8f312cf0e8cff6
Ashis Kumar Chanda <ashis415@yahoo.com>
2014-08-22 16:08:20 +0600

    Update 012_Training Options.R

--- 012_Training Options.R
+++ 012_Training Options.R
@@ -11,19 +11,11 @@ testing <- spam[-inTrain, ]
 
 modelFit <- train(type ~., data=training, method="glm")
 
-# train options
-args(train.default)
-
-# train control
-args(trainControl)
+args(train.default) # train options
+args(trainControl)  # train control
 
 # Setting the seed
 set.seed(1235)
 modelFit2 <- train(type ~., data=training, method="glm")
 modelFit2
 
-# Further information
-# Caret tutorials:
-#   - http://www.edii.uclm.es/~useR-2013/Tutorials/kuhn/user_caret_2up.pdf
-# Model training and tuning
-#   - http://caret.r-forge.r-project.org/training.html
